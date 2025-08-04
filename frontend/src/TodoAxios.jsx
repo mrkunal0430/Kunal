ead617ac8658ceae8b93084be35bc4bec933f168
Kunal Verma <mrkunal0430@gmail.com>
2025-08-04 14:58:27 +0530

    local to live site link for backend

--- frontend/src/TodoAxios.jsx
+++ frontend/src/TodoAxios.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 // Backend API base URL
-const API_URL = "http://localhost:5000/api/todos";
+const API_URL = "https://todoappdata.onrender.com/api/todos";
 
 function TodoAxios() {
     const [todos, setTodos] = useState([]);
