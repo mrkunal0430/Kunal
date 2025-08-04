ad95994a056ce50d498449d6b6863a2c9ebdbf46
Kunal Verma <mrkunal0430@gmail.com>
2025-08-04 14:53:12 +0530

    cors frontend link

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(cors({
-    origin: "https://your-vercel-frontend.vercel.app", // exact URL of frontend
+    origin: "https://todoapp-pjevbvrks-kunal-vermas-projects-48fd8a01.vercel.app/", // exact URL of frontend
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
